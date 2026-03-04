1b26185c28abe7987d219b0269c76f80298393cc
monirzkhan <odvutkabbo@gmail.com>
2026-03-04 08:25:29 +0600

    nav design done

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="navbar bg-base-100 shadow-sm md:px-8 md:py-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Home</a></li>
+                        <li><a>FAQ</a></li>
+                        <li><a>Changelog</a></li>
+                        <li><a>Blog</a></li>
+                        <li><a>Download</a></li>
+                        <li><a>Contact</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost md:text-2xl text-lg">FastFix-Ticket System</a>
+            </div>
+            
+            <div className="navbar-end">
+                <div className='hidden lg:flex '>
+                    <ul className="menu menu-horizontal px-1 text-xl">
+                    <li><a>Home</a></li>
+                    <li><a>FAQ</a></li>
+                    <li><a>Changelog</a></li>
+                    <li><a>Blog</a></li>
+                    <li><a>Download</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+                </div>
+                <div className='mx-8'>
+                    <button className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white p-2 md:px-6 md:py-4 rounded-lg">
+                        <i class="fa-solid fa-plus"></i>
+                        New Ticket </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;
